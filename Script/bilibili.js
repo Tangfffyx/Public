187b34d91dee48c5c54215307e2d1837d110d555
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2022-12-05 01:34:20 +0800

    Update bilibili.js

--- Script/bilibili.js
+++ Script/bilibili.js
@@ -1,4 +1,4 @@
-hostname=app.bilibili.com
+;hostname=app.bilibili.com
 
 # ～ BiliBili_哔哩哔哩_热搜发现@ddgksf2013
 ^https://app.bilibili.com/x/v2/search/square url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/bilibili_json.js
