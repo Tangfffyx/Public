76fe8e88bd768f05ad93ab94325ef2a0f9c04bd8
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2023-08-16 23:52:25 +0800

    Update testflight_auto_join.js

--- Rewrite/testflight_auto_join.js
+++ Rewrite/testflight_auto_join.js
@@ -11,14 +11,16 @@ hostname = testflight.apple.com
 使用方法：
 1、手动添加 cron 任务
 [task_local]
-*/10 * * * * * https://raw.githubusercontent.com/chouchoui/QuanX/master/Scripts/testflight/Auto_join_TF.js, tag=TestFlight自动加入, img-url= https://raw.githubusercontent.com/Orz-3/mini/master/Color/testflight.png, enabled=true
+*/10 * * * * * https://raw.githubusercontent.com/Tangfffyx/Public/main/Script/tf_auto_join.js, tag=TestFlight自动加入, img-url= https://raw.githubusercontent.com/Orz-3/mini/master/Color/testflight.png, enabled=true
 
-2、打开此重写
+2、需关闭TF多账户管理重写，并打开TestFlight一次，qx会提示信息获取成功
 
-3、复制想加入的链接（例如https://testflight.apple.com/join/fl3VSxsx）
+3、打开此重写
+
+4、复制想加入的链接（例如https://testflight.apple.com/join/fl3VSxsx）
 粘贴到浏览器打开，以获取 APP_ID
 
-4、获取 APP_ID 成功后，打开 cron 任务
+5、获取 APP_ID 成功后，打开 cron 任务
 
 最后：
    如何清除 APP_ID 持久化数据：
