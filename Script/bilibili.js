b3583b7676509035299f1aed354c5569eaa4e0a2
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2024-06-07 15:00:31 +0800

    Delete Script/bilibili.js

--- Script/bilibili.js
+++
@@ -1,6 +0,0 @@
-#作者@墨鱼
-
-;hostname=app.bilibili.com
-
-# ～ BiliBili_哔哩哔哩_热搜发现@ddgksf2013
-^https://app.bilibili.com/x/v2/search/square url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/bilibili_json.js
