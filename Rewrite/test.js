fffd0ba7396109a3a211a321f78e0a499841222e
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2024-07-15 17:10:01 +0800

    Update test.js

--- Rewrite/test.js
+++ Rewrite/test.js
@@ -1,4 +1,5 @@
-^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header ^GET\s([^\s]+)\sHTTP\/1\.1\r\nHost:\s([^\r\n]+)\r\n.*User-Agent:.*\r\n request-header GET $1 HTTP/1.1\r\nUser-Agent: SenPlayer/4.0.9\r\nAccept: */*\r\nRange: bytes=31985-\r\nConnection: close\r\nHost: $2\r\nIcy-MetaData: 1\r\n
+^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)X-Emby-Authorization:.+Client="VidHub_iOS",.+(\r\n) request-header $1X-Emby-Authorization: Emby UserId="5cafd4642e844414892fd340562d66a3", Client="SenPlayer", Device="iPad", DeviceId="3A32255D-B2EA-40B9-BD1C-D58592B77BC8", Version="1.7.2"$2
+^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)User-Agent:.+(\r\n) request-header $1User-Agent: SenPlayer/1.7.2$2
 
 [MITM]
 hostname = 9521732.xyz, *.9521732.xyz, embyplus.org, *.embyplus.org, chirsemby.top, *.chirsemby.top
