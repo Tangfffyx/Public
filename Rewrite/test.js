7901bf447ab0a0992a9933d07f9f00e67ade6187
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2024-07-15 16:30:59 +0800

    Update test.js

--- Rewrite/test.js
+++ Rewrite/test.js
@@ -1,4 +1,3 @@
-[Header Rewrite]
 http-request ^https?:\/\/(?:[^\/]+\.)?9521732\.xyz(:\d+)?\/ header-del User-Agent
 http-request ^https?:\/\/(?:[^\/]+\.)?9521732\.xyz(:\d+)?\/ header-add User-Agent "Emby"
 http-request ^https?:\/\/(?:[^\/]+\.)?embyplus\.org(:\d+)?\/ header-del User-Agent
@@ -7,4 +6,4 @@ http-request ^https?:\/\/(?:[^\/]+\.)?chirsemby\.top(:\d+)?\/ header-del User-Ag
 http-request ^https?:\/\/(?:[^\/]+\.)?chirsemby\.top(:\d+)?\/ header-add User-Agent "Emby"
 
 [MITM]
-hostname = %APPEND% 9521732.xyz, embyplus.org, chirsemby.top
+hostname = 9521732.xyz, *.9521732.xyz, embyplus.org, *.embyplus.org, chirsemby.top, *.chirsemby.top
