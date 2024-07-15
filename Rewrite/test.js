a0e54916b8bc5a8e0b78db060dfb9c79b78b38d6
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2024-07-15 17:16:20 +0800

    Update test.js

--- Rewrite/test.js
+++ Rewrite/test.js
@@ -1,5 +1,6 @@
-^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)X-Emby-Authorization:.+Client="VidHub_iOS",.+(\r\n) request-header $1X-Emby-Authorization: Emby UserId="5cafd4642e844414892fd340562d66a3", Client="SenPlayer", Device="iPad", DeviceId="3A32255D-B2EA-40B9-BD1C-D58592B77BC8", Version="1.7.2"$2
-^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)User-Agent:.+(\r\n) request-header $1User-Agent: SenPlayer/1.7.2$2
+^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)X-Emby-Authorization: Emby UserId="([^\"]+)", Client="VidHub_iOS", Device="([^\"]+)", DeviceId="([^\"]+)", Version="1.7.2"(\r\n) request-header $1X-Emby-Authorization: Emby UserId="$3", Client="SenPlayer", Device="$4", DeviceId="$5", Version="4.0.9"$6
+^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)User-Agent: VidHub\/2024071203 CFNetwork\/1496\.0\.7 Darwin\/23\.5\.0(\r\n) request-header $1User-Agent: SenPlayer/4.0.9$3
+
 
 [MITM]
 hostname = 9521732.xyz, *.9521732.xyz, embyplus.org, *.embyplus.org, chirsemby.top, *.chirsemby.top
