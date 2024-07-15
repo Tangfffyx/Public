0fba5aee01aac4950f60d0fd20f5db6e49d67d23
Tangfffyx <115964760+Tangfffyx@users.noreply.github.com>
2024-07-15 16:55:49 +0800

    Update test.js

--- Rewrite/test.js
+++ Rewrite/test.js
@@ -1,5 +1,4 @@
-^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header (\r\n)User-Agent:.+(\r\n) request-header $1User-Agent: SenPlayer/4.0.9$2
-
+^https?:\/\/(9521732\.xyz|.*\.9521732\.xyz|embyplus\.org|.*\.embyplus\.org|chirsemby\.top|.*\.chirsemby\.top)\/ url request-header ^GET\s.*\sHTTP/1\.1\r\n(.*\r\n)*User-Agent:.+\r\n(.*\r\n)*$ request-header GET $1 HTTP/1.1\r\nUser-Agent: SenPlayer/4.0.9\r\nAccept: */*\r\nRange: bytes=31985-\r\nConnection: close\r\nHost: $2\r\nIcy-MetaData: 1\r\n
 
 [MITM]
 hostname = 9521732.xyz, *.9521732.xyz, embyplus.org, *.embyplus.org, chirsemby.top, *.chirsemby.top
